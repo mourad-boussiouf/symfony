70f49539b91f14552d4448cc9fb63ce42f969f9b
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-09-22 15:30:56 +0200

    creation des controllers article category comment et page, mise a jour router argument second backslash contenu dans la variable twig slug. index function et twig renommer en show

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CommentController extends AbstractController
 {
-    #[Route('', name: 'app_home')]
+    #[Route('/comment', name: 'app_comment')]
     public function index(): Response
     {
-        return $this->render('home/show.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('comment/show.html.twig', [
+            'controller_name' => 'CommentController',
         ]);
     }
 }
