9501be4211c5d5370906f3a1cb01fed0b0d5d29d
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-09-25 19:32:11 +0200

    dropdown issu de bootstrap intégré au projet OK navigation grâce au dropdown js OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,9 @@
 import '../css/app.scss';
+import {Dropdown} from "bootstrap";
 
+const enableDropdowns = () => {
+   const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+   dropdownElementList.map(function (dropdownToggleEl){
+      return new Dropdown(dropdownToggleEl)
+       });
+}
